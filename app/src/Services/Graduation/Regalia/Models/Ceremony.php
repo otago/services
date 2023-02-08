428b4074815ea8ac05305229857119643aaccf97
Jeffrey Dick <jeffrey@jeffreydick.com>
2023-02-08 13:12:11 +1300

    Create data

--- app/src/Services/Graduation/Regalia/Models/Ceremony.php
+++ app/src/Services/Graduation/Regalia/Models/Ceremony.php
@@ -8,7 +8,19 @@ class Ceremony extends DataObject
 {
     private static $table_name = 'Services_Regalia_Models_Ceremony';
 
+    private static $db = [
+        'Title' => 'Varchar(255)',
+    ];
+
     private static $belongs_many_many = [
         "Submissions" => Submission::class
+    ];
+
+    private static $summary_fields = [
+        'ID' => 'ID',
+        'Title' => 'Title',
+        'Submissions.Count' => 'Submissions',
+        'Created' => 'Created',
+        'LastEdited' => 'Last Edited',
     ];
 }
--- app/src/Services/Graduation/Regalia/Models/Submission.php
+++ app/src/Services/Graduation/Regalia/Models/Submission.php
@@ -9,11 +9,32 @@ class Submission extends DataObject
 {
     private static $table_name = 'Services_Regalia_Models_Submission';
 
+    private static $db = [
+        'TrencherSize'          => 'Int',
+        'DeliveryLocation'      => 'Text',
+        'Comments'              => 'Text',
+        'Pickup'                => 'Boolean',
+        'GownRequired'          => 'Boolean',
+        'TrencherRequired'      => 'Boolean',
+        'GownLength'            => 'Varchar(255)',
+        'PreferredGown'         => 'Varchar(255)',
+        'GownRequirementsOther' => 'Varchar(255)',
+        'GownInstitutionOther'  => 'Varchar(255)',
+    ];
+
     private static $has_one = [
         'Member' => Member::class
     ];
 
     private static $many_many = [
         "Ceremonies" => Ceremony::class
+    ];
+
+    private static $summary_fields = [
+        'ID' => 'ID',
+        'Member.ID' => 'Member #ID',
+        'Ceremonies.Count' => 'Ceremonies',
+        'Created' => 'Created',
+        'LastEdited' => 'Last Edited',
     ];
 }
