188610f07146b7b854a4649885c23391028ca071
Jeffrey Dick <jeffrey@jeffreydick.com>
2023-02-09 16:23:26 +1300

    Sync up graphql

--- app/Services/Graduation/Regalia/client/js/app.js
+++ app/Services/Graduation/Regalia/client/js/app.js
@@ -1,3 +1,5 @@
+import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache } from '@apollo/client/core';
+import { createApolloProvider } from '@vue/apollo-option';
 import { createApp } from 'vue';
 import RegaliaForm from './components/RegaliaForm.vue';
 import getToken from './components/getToken';
@@ -6,8 +8,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const appElement = document.getElementById('app');
     const data = Object.assign({}, appElement.dataset);
     const xCsrfToken = getToken(data);
-    console.log(xCsrfToken);
-
+    if (!xCsrfToken) {
+        return;
+    }
+    const cache = new InMemoryCache();
+    const link = createHttpLink({
+        uri: data.graphqlUrl,
+    });
+    const authMiddleware = new ApolloLink((operation, forward) => {
+        operation.setContext({
+            headers: {
+                "X-CSRF-TOKEN": data.xCsrfToken
+            }
+        });
+        return forward(operation);
+    })
+    const apolloClient = new ApolloClient({
+        link: concat(authMiddleware, link),
+        cache,
+    });
+    const apolloProvider = createApolloProvider({
+        defaultClient: apolloClient,
+    })
     const app = createApp(RegaliaForm);
+    app.use(apolloProvider);
     app.mount(appElement);
 });
